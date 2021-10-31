5b17265ba0e4f56cb93c3ee54aee01951aa5cf3a
Ben Attenborough <battenborough@gmail.com>
2021-10-31 22:12:03 +0000

    Work on state management

--- states/MenuState.lua
+++ states/MenuState.lua
@@ -3,20 +3,41 @@ MenuState = Class{__includes = BaseState}
 require 'objects.Button'
 
 function MenuState:init()
-    -- self.saveButton = Button(20,200,100,14,'Save',self.saveHandler)
-    -- self.loadButton = Button(20,225,100,14,'Load',self.loadHandler)
+    self.playButton = Button(20,200,100,14,'Play',self.switchToPlayState)
+    self.editButton = Button(20,225,100,14,'Edit',self.switchToEditState)
+    self.prevMouseDown = false
 end
 
 function MenuState:update(dt)
-    
+    if love.mouse.isDown(1) and not self.prevMouseDown then
+        local x,y = push:toGame(love.mouse.getX(), love.mouse.getY())
+        -- handle click
+        self.playButton:mouseClick(x, y, 1)
+        self.editButton:mouseClick(x, y, 1)
+    end
+    self.prevMouseDown = love.mouse.isDown(1)
 end
 
 function MenuState:render()
-
+    self.playButton:draw()
+    self.editButton:draw()
 end
 
 function MenuState:exit() end
 
 function MenuState:mousereleased(x, y, button)
     MapEditor:mousereleased(x, y, button)
+end
+
+function MenuState:switchToPlayState()
+    gStateMachine:change('play', {})
+end
+
+function MenuState:switchToEditState()
+    gStateMachine:change('edit', {})
+end
+
+function MenuState:mousereleased(x, y, button)
+    self.playButton:mousereleased(x, y, button)
+    self.editButton:mousereleased(x, y, button)
 end
