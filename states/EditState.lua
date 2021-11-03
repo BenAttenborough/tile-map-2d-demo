9858e00ee435f5fee6d009851946d5976fc19f77
Ben Attenborough <battenborough@gmail.com>
2021-11-03 22:46:01 +0000

    WIP: Move functionality out of the demo and into the map editor library

--- states/EditState.lua
+++ states/EditState.lua
@@ -1,52 +1,21 @@
 EditState = Class{__includes = BaseState}
 
-require 'libs.tilemap2d.tilemap2d'
+require 'libs.tilemap2d.MapEditor'
 
 function EditState:init()
-    self.prevMouseDown = false
-    self.mapLoaded = false
-    self.loadButton = Button(20,200,100,14,'Load','load',self)
-    self.saveButton = Button(20,225,100,14,'Save','save',self)
+    self.MapEditor = MapEditor()
 end
 
 function EditState:update(dt)
-    if love.mouse.isDown(1) and not self.prevMouseDown then
-        local x,y = Push:toGame(love.mouse.getX(), love.mouse.getY())
-        self.loadButton:mouseClick(x, y, 1)
-        self.saveButton:mouseClick(x, y, 1)
-    end
-    self.prevMouseDown = love.mouse.isDown(1)
+    self.MapEditor:update(dt)
 end
 
 function EditState:render()
-    self.loadButton:draw()
-    self.saveButton:draw()
-    if self.mapLoaded then
-        Tilemap2d:draw()
-    end
+    self.MapEditor:render()
 end
 
 function EditState:exit() end
 
-function EditState:load()
-    local tileConfig = {}
-    tileConfig['map'] = map
-    tileConfig['spriteSheet'] = 'sprites/tilemap.png'
-    tileConfig['spriteSize'] = {}
-    tileConfig['spriteSize']['width'] = 10
-    tileConfig['spriteSize']['height'] = 10
-    tileConfig['spriteCount'] = 4
-    tileConfig['offsetX'] = 20
-    tileConfig['offsetY'] = 50
-    Tilemap2d = TileMap2d(tileConfig)
-    self.mapLoaded = true
-end
-
-function EditState:save()
-    print("Would be saving")
-end
-
 function EditState:mousereleased(x, y, button)
-    self.loadButton:mousereleased(x, y, button)
-    self.saveButton:mousereleased(x, y, button)
+    self.MapEditor:mousereleased(x, y, button)
 end
